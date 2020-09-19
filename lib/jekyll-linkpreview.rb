61983e8c58031ecf95a00ad967e9893c17ac0785
Mostafa Moradian <mostafamoradian0@gmail.com>
2020-09-19 23:21:28 +0200

    Add modified jekyll-resize filter to download the image to cache and resize it

--- lib/jekyll-linkpreview.rb
+++ lib/jekyll-linkpreview.rb
@@ -1,18 +1,82 @@
 require "digest"
 require "json"
 require 'uri'
-
+require 'open-uri'
+require 'digest'
+require 'mini_magick'
 require "metainspector"
 require "jekyll-linkpreview/version"
 
 module Jekyll
+  module Resize
+    def resize(source, options)
+      site = @context.registers[:site]
+
+      if !File.exist?(source)
+        download(source)
+      end
+
+      source_path = site.source + source
+      raise '#{source_[path]} is not readable' unless File.readable?(source_path)
+
+      destination_path = '/cache/resize/'
+      destination = site.source + destination_path
+
+      FileUtils.mkdir_p destination
+
+      ext = File.extname(source)
+      desc = options.gsub(/[^\da-z]+/i, '')
+
+      sha = Digest::SHA256.file source_path
+
+      destination_file_name = '#{sha}_#{desc}#{ext}'
+      destination += destination_file_name
+
+      if !File.exist?(destination) || File.mtime(destination) <= File.mtime(source_path)
+
+        puts 'Thumbnailing #{source_path} to #{destination} (#{options})'
+
+        image = MiniMagick::Image.open(source_path)
+        image.strip
+        image.resize options
+        image.write destination
+        site.static_files << Jekyll::StaticFile.new(site, site.source, destination_path, destination_file_name)
+      end
+
+      destination_path + destination_file_name
+    end
+
+    def download(source)
+      filename = File.basename(url)
+
+      destination_path = '/cache/downloads/'
+      destination = site.source + destination_path
+      destination_file_name = destination_path + filename
+
+      if !Dir.exists?(destination)
+        FileUtils.mkdir_p destination
+      end
+
+      if !File.exist?(destination_file_name)
+        URI.open(url) do |image|
+          File.open(destination, 'wb') do |file|
+            puts 'Downloading #{filename} to #{destination_file_name}'
+            file.write(image.read)
+          end
+        end
+      end
+
+      return destination_file_name
+    end
+  end
   module Linkpreview
     class OpenGraphProperties
       def get(url)
         page = fetch(url)
         og_properties = page.meta_tags['property']
         og_url = get_og_property(og_properties, 'og:url')
-        image_url = get_og_property(og_properties, 'og:image')
+        og_image_url = get_og_property(og_properties, 'og:image')
+        image_url = Resize::resize(image_url, "100x100>")
         {
           'title'       => get_og_property(og_properties, 'og:title'),
           'url'         => og_url,
